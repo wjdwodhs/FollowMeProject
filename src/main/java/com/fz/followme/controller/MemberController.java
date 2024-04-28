934539bff46f546c2a1e98087c695a036e4f129c
DayeLee <regina2504@naver.com>
2024-04-28 18:16:14 +0900

    modify : MemberController 수정
    modify : MemberController 수정
    author : 이다예
    - 마이페이지로 이동, 로그아웃 기능 추가

--- src/main/java/com/fz/followme/controller/MemberController.java
+++ src/main/java/com/fz/followme/controller/MemberController.java
@@ -6,9 +6,11 @@ import java.io.PrintWriter;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -77,4 +79,17 @@ public class MemberController {
 		
 	}
 	
+	// 마이페이지로 이동
+	@RequestMapping("/mypage.page")
+	public String mypage() {
+		return "member/mypage";
+	}
+	
+	
+	// 로그아웃
+	@RequestMapping("/logout.do")
+	public String logout(HttpSession session) {
+		session.invalidate();
+		return "redirect:/";
+	}
 }
