2a1c035cffdeffcb4c1b3b7d7265594212d653b3
dandi0624 <153481807+dandi0624@users.noreply.github.com>
2024-04-29 16:23:18 +0900

    modify : HomeController
    - 자산관리 url 추가

--- src/main/java/com/fz/followme/controller/HomeController.java
+++ src/main/java/com/fz/followme/controller/HomeController.java
@@ -31,4 +31,24 @@ public class HomeController {
 	public String attendancePage() {
 		return "attendance/attendance";
 	}
+	
+	// * 자산관리페이지 관련 ---------------------------
+	@RequestMapping("/carsReservation.page")
+	public String carsReservation() {
+		return "assetManagement/carsReservation";
+	}
+	@RequestMapping("/carsReservationManager.page")
+	public String carsReservationManager() {
+		return "assetManagement/carsReservationManager";
+	}
+	@RequestMapping("/seatReservation.page")
+	public String seatReservation() {
+		return "assetManagement/seatReservation";
+	}
+	@RequestMapping("/seatReservationManager.page")
+	public String seatReservationManager() {
+		return "assetManagement/seatReservationManager";
+	}
+	
+	//----------------------------------------------
 }
