88ee99c94b3f52b92edef70673dd045c7facc13d
DayeLee <regina2504@naver.com>
2024-05-01 16:54:37 +0900

    modify : ceoMain.jsp 뉴스란 추가 관련 수정
    modify : ceoMain.jsp 뉴스란 추가 관련 수정
    author : 이다예
    - 패션 뉴스란 추가 관련 수정

--- src/main/java/com/fz/followme/controller/ChartController.java
+++ src/main/java/com/fz/followme/controller/ChartController.java
@@ -1,15 +1,19 @@
 package com.fz.followme.controller;
 
+import java.net.URI;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestTemplate;
 
-import com.fz.followme.dto.MemberDto;
 import com.fz.followme.dto.OrderDto;
-import com.fz.followme.service.MemberService;
 import com.fz.followme.service.OrderService;
 
 import lombok.RequiredArgsConstructor;
@@ -20,7 +24,6 @@ import lombok.RequiredArgsConstructor;
 public class ChartController {
 	
 	private final OrderService orderService;
-	private final MemberService memberService;
 
 	@ResponseBody
 	@GetMapping("/revenue.do")
@@ -28,4 +31,27 @@ public class ChartController {
 		return orderService.selectOrder();
 	}
 	
+	@ResponseBody
+	@GetMapping("/naverNews.do")
+	public ResponseEntity<String> getNaverNews() {
+        String clientId = "Icsu9Z4AFgHEk4gCROsJ"; // 애플리케이션 클라이언트 아이디
+        String clientSecret = "v7cwu7ppik"; // 애플리케이션 클라이언트 시크릿
+        String query = "패션"; // 검색어
+
+        String encodedQuery = java.net.URLEncoder.encode(query, StandardCharsets.UTF_8);
+        String apiUrl = "https://openapi.naver.com/v1/search/news.json?query=" + encodedQuery + "&display=100&start=1&sort=date";
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("X-Naver-Client-Id", clientId);
+        headers.add("X-Naver-Client-Secret", clientSecret);
+
+        RequestEntity<Void> req = RequestEntity
+                .get(URI.create(apiUrl))
+                .headers(headers)
+                .build();
+
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> response = restTemplate.exchange(req, String.class);
+        return response;
+    }
 }
