f623423dfa8e674ce12bd8e97a8dfa0fd2d399d4
dandi0624 <153481807+dandi0624@users.noreply.github.com>
2024-05-09 11:38:14 +0900

    add & modify : dto 추가 및 config 수정

---
+++ src/main/java/com/fz/followme/dto/AssetReservationDto.java
@@ -0,0 +1,31 @@
+package com.fz.followme.dto;
+
+import java.sql.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@ToString
+@Builder
+public class AssetReservationDto {
+	
+	private int rsvnNo;
+	private int assetNo;
+	private String rsvnName;
+	private Date rsvnDate;
+	private String startDivision;
+	private Date startDate;
+	private String endDivision;
+	private Date endDate;
+	private String rsvnContent;
+	private String status;
+	
+}
