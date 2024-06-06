4883ba9e8549b06367b91c85a5d9ad615a159328
dandi0624 <153481807+dandi0624@users.noreply.github.com>
2024-06-06 18:49:28 +0900

    modify : scheduler 수정
    author:김민정

--- src/main/java/com/fz/followme/scheduler/ReservationScheduler.java
+++ src/main/java/com/fz/followme/scheduler/ReservationScheduler.java
@@ -13,6 +13,7 @@ import com.fz.followme.service.ReservationService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+
 @Slf4j
 @RequiredArgsConstructor
 @Component
@@ -22,37 +23,30 @@ public class ReservationScheduler {
 	private final List<String> assetNames;
 	
 	
+	
+	/**
+	 * @author 김민정
+	 * 차량예약 시간 시작시 status 상태값 update
+	 *
+	 */
 	@Scheduled(cron = "0 0 * * * *") // 자정에서부터 1시간마다 실행
 	public void updateAssetstartDateStatus() {
 		int result = reservationService.updateAssetstartDateStatus();
 		log.debug("시작일 업데이트 결과: {}", result);
 	}
 	
-	
+	/**
+	 * @author 김민정
+	 * 차량예약 시간 종료시 status 상태값 update
+	 *
+	 */
 	@Scheduled(cron = "0 0 * * * *")  
 	public void checkUpdateAssetStatus() {
 		int result = reservationService.updateAssetendDateStatus();
 		log.debug("종료일 업데이트 결과: {}", result);
 	}
 	
-	
-	/*
-	@Scheduled(cron = "0 0 * * * *")
-	public void updateStatusSeat() {
-		
-		String todayDate = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
-		
-		for(String assetName : assetNames) {
-			AssetDto ad = new AssetDto();
-			ad.setAssetName(assetName);
-			ad.setModifyDate(todayDate);
-			
-			int result = reservationService.updateSeatStatus(ad);
-			log.debug("좌석상태 업데이트 결과 : {}{}", assetName, result);
-		}
-		
-	}
-	*/
+
 	
 	
 }
